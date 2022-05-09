a987b26e46b4550ed46a32360202f3430abc336b
chenpeng <398753940@qq.com>
2022-05-09 14:50:07 +0800

    peizhi

--- backend/conf/env.py
+++ backend/conf/env.py
@@ -12,10 +12,10 @@ DATABASE_ENGINE = "django.db.backends.sqlite3"
 DATABASE_NAME = os.path.join(BASE_DIR, 'db.sqlite3')
 
 # 使用mysql时，改为此配置
-# DATABASE_ENGINE = "django.db.backends.mysql"
-# DATABASE_NAME = 'django-vue-admin' # mysql 时使用
+DATABASE_ENGINE = "django.db.backends.mysql"
+DATABASE_NAME = 'django' # mysql 时使用
 # 数据库地址 改为自己数据库地址
-DATABASE_HOST = "127.0.0.1"
+DATABASE_HOST = "118.24.203.63"
 # # 数据库端口
 DATABASE_PORT = 3306
 # # 数据库用户名
